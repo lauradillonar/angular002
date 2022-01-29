c1b089d641fb8eb7bfdfe14ad58618f6944ffd37
lauradillonar <laura.dillon.ar@gmail.com>
2022-01-29 16:17:15 -0300

    Interpolacion y operador ternario - Curso Angular PildorasInformaticas

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -8,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
   //styles:["p {background-color: antiquewhite;}"]
 })
 export class EmpleadoComponent implements OnInit {
+  nombre="Juan";
+  apellido="Díaz";
+  //private edad=18;
+  edad=18;
+  empresa="Píldoras Informáticas";
+
+  /*
+  getEdad(){
+    return this.edad;
+  }
+  */
 
   constructor() { }
 
