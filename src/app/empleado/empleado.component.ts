d0d5dfe0c55ba22106923592a05f61e02efad409
lauradillonar <laura.dillon.ar@gmail.com>
2022-01-30 00:41:13 -0300

    Ejemplo de interpolacion - Curso PildorasInformaticas

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,22 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
-  //template: "<p>Aquí va un empleado</p>",
   styleUrls: ['./empleado.component.css']
-  //styles:["p {background-color: antiquewhite;}"]
 })
 export class EmpleadoComponent implements OnInit {
   nombre="Juan";
   apellido="Díaz";
-  //private edad=18;
   edad=18;
-  empresa="Píldoras Informáticas";
+  //empresa="Píldoras Informáticas";
 
-  /*
-  getEdad(){
-    return this.edad;
-  }
-  */
+  //llamaEmpresa(value:string){}
 
   constructor() { }
 
